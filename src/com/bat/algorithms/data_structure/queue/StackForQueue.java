196a5111ae23f490c6f91a0ce1c57c1b86be7e02
qiupingchen <qiupingchen@tencent.com>
2021-03-22 15:18:24 +0800

    使用栈实现队列和使用队列实现栈

---
+++ src/com/bat/algorithms/data_structure/queue/StackForQueue.java
@@ -0,0 +1,51 @@
+package bat.algorithms.data_structure.queue;
+
+import java.util.Stack;
+
+public class StackForQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public StackForQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void EnQueue(int x) {
+        stack1.push(x);
+    }
+
+    public int DeQueue() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int GetHead() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean QueueEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        StackForQueue sfq = new StackForQueue();
+        sfq.EnQueue(1);
+        sfq.EnQueue(2);
+        sfq.EnQueue(3);
+        sfq.EnQueue(4);
+        sfq.EnQueue(5);
+        while (!sfq.QueueEmpty()) {
+            System.out.println(sfq.DeQueue());
+        }
+    }
+}
---
+++ src/com/bat/algorithms/data_structure/stack/QueueForStack.java
@@ -0,0 +1,50 @@
+package bat.algorithms.data_structure.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueForStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public QueueForStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    //queue1除了push操作的时候均为空
+    public void Push(int x) {
+        queue1.offer(x);
+        while (!queue2.isEmpty()) {
+            queue1.offer(queue2.poll());
+        }
+        Queue<Integer> temp = queue2;
+        queue2 = queue1;
+        queue1 = temp;
+    }
+
+    public int Pop() {
+        return queue2.poll();
+    }
+
+    public int GetTop() {
+        return queue2.peek();
+    }
+
+    public boolean QueueEmpty() {
+        return queue2.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        QueueForStack qfs = new QueueForStack();
+        qfs.Push(1);
+        qfs.Push(2);
+        qfs.Push(3);
+        qfs.Push(4);
+        qfs.Push(5);
+        while (!qfs.QueueEmpty()) {
+            System.out.println(qfs.Pop());
+        }
+    }
+
+}
