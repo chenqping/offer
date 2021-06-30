84fd8d9b2b031474fa2391599069ce0e38520293
qiupingchen <qiupingchen@tencent.com>
2021-06-30 15:11:17 +0800

    改变链表遍历打印为输出列表

--- src/com/bat/algorithms/data_structure/linked_list/LinkedListOps.java
+++ src/com/bat/algorithms/data_structure/linked_list/LinkedListOps.java
@@ -1,50 +1,83 @@
 package bat.algorithms.data_structure.linked_list;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * 链表基本操作
-* */
+ */
 
 public class LinkedListOps {
 
     public static void main(String[] args) {
-        ListNode pHead1 = constructLinkList(5);
-        printLinkInNormalOrder(pHead1.next);
-        printLinkInReverseOrder(pHead1.next);
-
-        ListNode pHead2 = constructLinkList(0);
-        printLinkInNormalOrder(pHead2.next);
-        printLinkInReverseOrder(pHead2.next);
+        ListNode l1 = createLinkedList();
+        List<Integer> list = traverseList(l1);
+        System.out.println(list);
+        list = reverseTraverseListRecursively(l1);
+        System.out.println(list);
+        list = reverseTraverseListNonrecursively(l1);
+        System.out.println(list);
     }
 
-    public static ListNode constructLinkList(int num) {
-        ListNode pHead = new ListNode(num);
-        ListNode pNode = pHead;
-        for (int i = 0; i < num; i++) {
-            ListNode node = new ListNode(i);
-            pNode.next = node;
-            pNode = node;
+    public static ListNode createLinkedList() {
+        ListNode pHead = null, pNode = null;
+        boolean isHead = true;
+        Scanner scanner = new Scanner(System.in);
+        //输入完毕后回车并Ctrl+D结束输入，一定要回车，不然前一个回车之后的输入都将丢失!
+        while (scanner.hasNextInt()) {
+            int value = scanner.nextInt();
+            ListNode node = new ListNode(value);
+            if (isHead) {
+                pNode = pHead = node;
+                isHead = false;
+            }else{
+                pNode.next = node;
+                pNode = pNode.next;
+            }
         }
+        scanner.close();
         return pHead;
     }
 
-    public static void printLinkInNormalOrder(ListNode pHead) {
-        ListNode pNode = pHead;
-        while (pNode != null) {
-            System.out.println(pNode.val);
-            pNode = pNode.next;
+    public static void print(ListNode pHead) {
+        ListNode node = pHead;
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
         }
     }
 
-    /*
-     *    实现链表的逆序输出
-     *    边界条件：
-     *    1. 功能测试：
-     *    2. 特殊输入测试，如空链表
-     * */
-    public static void printLinkInReverseOrder(ListNode pHead) {
-        if (pHead != null) {
-            printLinkInReverseOrder(pHead.next);
-            System.out.println(pHead.val);
+    public static List<Integer> traverseList(ListNode pHead) {
+        List<Integer> list = new ArrayList<>();
+        ListNode pNode = pHead;
+        while (pNode != null){
+            list.add(pNode.val);
+            pNode = pNode.next;
         }
+        return list;
+    }
+
+    public static List<Integer> reverseTraverseListRecursively(ListNode pHead) {
+        List<Integer> list = new ArrayList<>();
+        if(pHead != null){
+            list.addAll(reverseTraverseListNonrecursively(pHead.next));
+            list.add(pHead.val);
+        }
+        return list;
+    }
+
+    public static List<Integer> reverseTraverseListNonrecursively(ListNode pHead) {
+        List<Integer> list = new ArrayList<>();
+        ListNode pTail = null;
+        while (pTail != pHead){
+            ListNode pNode = pHead;
+            while (pNode.next != pTail){
+                pNode = pNode.next;
+            }
+            pTail = pNode;
+            list.add(pTail.val);
+        }
+        return list;
     }
 }
--- src/com/bat/algorithms/data_structure/linked_list/ListNode.java
+++ src/com/bat/algorithms/data_structure/linked_list/ListNode.java
@@ -1,10 +1,14 @@
 package bat.algorithms.data_structure.linked_list;
 
-
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) {
-        val = x;
+    ListNode(){}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
